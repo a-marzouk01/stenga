e9988843aa47fb52c706d5cbeb735769f2f85248
a-marzouk01 <a.marzou01@outlook.com>
2025-08-31 22:20:43 +0300

    adding the main idea of the program and a basic simulation

---
+++ main.c
@@ -0,0 +1,144 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    unsigned int capacity;
+    unsigned int size;
+
+    int* items;
+} Dray;
+
+void append(Dray *arr, int num) { 
+    assert(arr->size <= arr->capacity);
+
+    if (arr->size == arr->capacity) {
+        int new_size = arr->capacity * 2;
+		if (arr->capacity == 0) {
+			new_size = 256;
+		}
+        int *new_start = realloc(arr->items, new_size * sizeof(int));
+
+        if (new_start == NULL) {
+            perror("Failed to reallocate memory");
+            free(arr->items);
+            exit(1);
+        }
+
+        arr->items = new_start;
+        arr->capacity = new_size;
+    }
+
+	arr->items[arr->size] = num;
+    arr->size++;
+}
+
+int pop(Dray *arr) { 
+    assert(arr->capacity > 0);
+    
+    int val = arr->items[arr->size-1];
+    arr->size--;
+    if (arr->size < arr->capacity/2) {
+        int new_size = arr->capacity / 2;
+        int *new_start = realloc(arr->items, new_size * sizeof(int));
+
+        if (new_start == NULL) {
+            perror("Failed to reallocate memory");
+            free(arr->items);
+            exit(1);
+        }
+
+        arr->items = new_start;
+        arr->capacity = new_size;
+    }
+    return val;
+}
+
+Dray construct(unsigned int size) {
+    Dray arr;
+    arr.capacity = size;
+    arr.size = 0;
+    arr.items = malloc(size * sizeof(int));
+
+    if (arr.items == NULL) {
+        perror("Failed to allocate memory");
+        free(arr.items);
+        exit(1);
+    }
+
+    return arr;
+}
+
+enum OPs {
+    PUSH,
+    PLUS,
+    DUMP
+};
+
+Dray operations;
+Dray operands;
+
+void add_op(enum OPs op, int n) {
+    append(&operations, op);
+    append(&operands, n);
+}
+
+void populate_arr() {
+    // TODO: add a full-on lexer to populate the array
+    add_op(PUSH, 34);
+    add_op(PUSH, 35);
+    add_op(PLUS, 0);
+    add_op(DUMP, 0);
+}
+
+void simulate_program() {
+    Dray st = construct(1);
+    for (int i = 0; i < operations.size; i++) {
+        if (operations.items[i] == PUSH) {
+            append(&st, operands.items[i]);
+        } else if (operations.items[i] == PLUS) {
+            assert(st.size >= 2);
+
+            int o1 = st.items[st.size-1];
+            pop(&st);
+
+            int o2 = st.items[st.size-1];
+            pop(&st);
+
+            append(&st, o1 + o2);
+        } else {
+            assert(st.size >= 1);
+
+            int o1 = st.items[st.size-1];
+            pop(&st);
+
+            printf("%d\n", o1);
+        }
+    }
+}
+
+void compile_program() {
+    // TODO: IMPLEMENT
+    printf("NOT IMPLEMENTED\n");
+    return;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        printf("USAGE: stenga [sub-command]");
+        return 1;
+    }
+
+    operations = construct(1);
+    operands = construct(1);
+    populate_arr();
+
+    if (strcmp(argv[1], "sim") == 0) {
+        simulate_program();
+    } else if (strcmp(argv[1], "com") == 0) {
+        compile_program();
+    }
+
+    return 0;
+}
